078bbb8bca49396534c6b9ffa2bc713a6159e232
Abdul Sami <rajasami408@gmail.com>
2024-08-10 15:19:14 +0500

    done

--- main.py
+++ main.py
@@ -162,27 +162,31 @@ def get_random_image():
 if st.button("Submit"):
     random_image = get_random_image()
     with st.spinner("Processing..."):
-        dalle_image_url = post_image_request_dalle(dalle_text)
-        if dalle_image_url:
-            st.title("Idea 2")
-            st.image(dalle_image_url)
-        stable_image_id = post_image_request_stable_diffusion(random_image, stable_text)
-        if stable_image_id:
-            stable_image = get_stable_image(stable_image_id)
-            if stable_image:
-                st.title("Idea 1")
-                st.image(stable_image)
-        leo_image_url = process_image_leo(random_image, leonardo_text, LEONARD_API_KEY)
-        if leo_image_url:
-                st.title("Idea 3")
-                st.image(leo_image_url)
+        if dalle_text:
+            dalle_image_url = post_image_request_dalle(dalle_text)
+            if dalle_image_url:
+                st.title("Idea 2")
+                st.image(dalle_image_url)
+        if stable_text:
+            stable_image_id = post_image_request_stable_diffusion(random_image, stable_text)
+            if stable_image_id:
+                stable_image = get_stable_image(stable_image_id)
+                if stable_image:
+                    st.title("Idea 1")
+                    st.image(stable_image)
+        if leonardo_text:
+            leo_image_url = process_image_leo(random_image, leonardo_text, LEONARD_API_KEY)
+            if leo_image_url:
+                    st.title("Idea 3")
+                    st.image(leo_image_url)
+        if mid_journey_text:
 
-        message_id = post_image_request_midjourney(f"{random_image} {mid_journey_text}")
-        if message_id:
-            image_url = get_image(message_id)
-            if image_url:
-                st.title("Idea 4")
-                st.image(image_url)
+            message_id = post_image_request_midjourney(f"{random_image} {mid_journey_text}")
+            if message_id:
+                image_url = get_image(message_id)
+                if image_url:
+                    st.title("Idea 4")
+                    st.image(image_url)
     
 
 
